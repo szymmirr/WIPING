cf16c46275b80680cf13b3a5c219b2ac98465bb1
szymmirr <szymmirr@wp.pl>
2021-06-15 10:34:17 +0200

    Add WIPING2 tests

---
+++ WIPING2/tests/WIPING27-test.cpp
@@ -0,0 +1,41 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+#define main __real_main
+#include "../WIPING27.cpp"
+#undef main
+
+std::string INPUT_1 = "Kakaj AABB zupka";
+std::string CORRECT_ANSWER_1 = "110";
+std::string INCORRECT_ANSWER_1 = "100";
+
+std::ostringstream capture_main_stdout(std::string test_input) {
+    std::istringstream input(test_input);
+    std::cin.rdbuf(input.rdbuf());
+
+    std::ostringstream output;
+    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(output.rdbuf());
+
+    __real_main();
+    std::cout.rdbuf(p_cout_streambuf);
+
+    return output;
+}
+
+TEST_CASE("Incorrect return") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    REQUIRE_FALSE(__real_main() == 1);
+}
+
+TEST_CASE("Incorrect answer") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    INFO(output.str());
+	REQUIRE_FALSE(output.str() == INCORRECT_ANSWER_1);
+}
+
+TEST_CASE("Correct answer 1") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    INFO(output.str());
+	REQUIRE(output.str() == CORRECT_ANSWER_1);
+}
---
+++ WIPING2/tests/WIPING2A-test.cpp
@@ -0,0 +1,46 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+#define main __real_main
+#include "../WIPING2A.cpp"
+#undef main
+
+std::string INPUT_1 = "11\n12\n132";
+std::string CORRECT_ANSWER_1 = "2\n2";
+std::string INCORRECT_ANSWER_1 = "0\n0";
+
+std::ostringstream capture_main_stdout(std::string test_input) {
+    std::istringstream input(test_input);
+    std::cin.rdbuf(input.rdbuf());
+
+    std::ostringstream output;
+    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(output.rdbuf());
+
+    __real_main();
+    std::cout.rdbuf(p_cout_streambuf);
+
+    return output;
+}
+
+TEST_CASE("Incorrect return") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    REQUIRE_FALSE(__real_main() == 1);
+}
+
+TEST_CASE("Correct return") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    REQUIRE(__real_main() == 0);
+}
+
+TEST_CASE("Incorrect answer") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    INFO(output.str());
+	REQUIRE_FALSE(output.str() == INCORRECT_ANSWER_1);
+}
+
+TEST_CASE("Correct answer 1") {
+    std::ostringstream output = capture_main_stdout(INPUT_1);
+    INFO(output.str());
+	REQUIRE(output.str() == CORRECT_ANSWER_1);
+}
